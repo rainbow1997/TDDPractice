68635e66f70763fbf124583eee1da7a42d620d76
Mostafa Jamali <rainbow1997@protonmail.com>
2023-04-23 19:33:00 +0330

    use setup method

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -1,42 +1,57 @@
 <?php
 
 namespace Tests;
+
 use Smost\Jargon\TagParser;
 use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
     //data like : product,general
+    protected TagParser $parser;
+
+    public function setup(): void //based on TestCase class it's run over all over the single test
+    {
+        //ARRANGE
+        $this->parser = new TagParser();
+    }
 
     public function testItParsesASingleTag()
     {
-        $parser = new TagParser;
-        $result = $parser->parse('personal');
+        //ACT
+        $result = $this->parser->parse('personal');
         $expectedOutput = ['personal'];
+        //ASSERT
         $this->assertSame($expectedOutput, $result);
 
     }
+
     public function testItParsesACommaSeparatedListOfTags()
     {
-        $parser = new TagParser;
-        $result = $parser->parse('personal, money, family');
-        $expectedOutput = ['personal','money','family'];
+        //ACT
+        $result = $this->parser->parse('personal, money, family');
+        $expectedOutput = ['personal', 'money', 'family'];
+        //ASSERT
         $this->assertSame($expectedOutput, $result);
 
     }
+
     public function testCommasAreOptional()
     {
-        $parser = new TagParser;
-        $result = $parser->parse('personal,money,family');
-        $expectedOutput = ['personal','money','family'];
+        //ACT
+        $result = $this->parser->parse('personal,money,family');
+        $expectedOutput = ['personal', 'money', 'family'];
+        //ASSERT
         $this->assertSame($expectedOutput, $result);
 
     }
+
     public function testItParsesAPipeSeparatedListOfTags()
     {
-        $parser = new TagParser;
-        $result = $parser->parse('personal|money|family|shop');
-        $expectedOutput = ['personal','money','family','shop'];
+        //ACT
+        $result = $this->parser->parse('personal|money|family|shop');
+        $expectedOutput = ['personal', 'money', 'family', 'shop'];
+        //ASSERT
         $this->assertSame($expectedOutput, $result);
     }
 }
